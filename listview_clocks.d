883c708633b50c015a8763aab5e1bb6fede9bae4
passenger94 <pierredavant@gmail.com>
2025-04-28 18:52:19 +0200

    Update listview_clocks.d

--- listview_clocks.d
+++ listview_clocks.d
@@ -4,218 +4,19 @@ import std.logger;
 import std.typecons : Yes;
 import std.algorithm : min;
 
-import gobject.types, gobject.object, gobject.value, gobject.closure, gobject.cclosure;
+import gobject.types, gobject.object, gobject.value;
 import gio.types : ApplicationFlags;
 import gio.simple_action, gio.application : ApplicationGio = Application;
 import gio.list_store, gio.list_model;
 import glib.time_zone, glib.date_time, glib.variant, glib.global;
 import gtk.types, gtk.widget, gtk.application, gtk.application_window, gtk.scrolled_window, gtk.box, gtk.label, 
-       gtk.picture, gtk.list_item, gtk.signal_list_item_factory, gtk.no_selection, gtk.grid_view, gtk.snapshot, 
-       gtk.expression, gtk.constant_expression, gtk.property_expression, gtk.closure_expression;
+       gtk.picture, gtk.list_item, gtk.signal_list_item_factory, gtk.no_selection, gtk.grid_view;;
 import gdk.paintable, gdk.rgba;
 import gsk.rounded_rect;
 import graphene.point, graphene.rect;
 
+import clocks;
 
-class GtkClock : ObjectWrap
-{
-    mixin(objectMixin);
-    
-    static GtkClock[] ticking_clocks;
-    static uint ticking_clock_id;
-
-    /* Name of the location we're displaying time for */
-    string location;
-    ///
-    DateTime time;
-    /* We allow this to be null for the local timezone */
-    TimeZone timezone;
-
-    ListStore store;
-    Paintable paintable;
-    string time_format;
-    gtk.snapshot.Snapshot snap;
-
-    this(string location, TimeZone tz, ListStore store)
-    {
-        super(GTypeEnum.Object);
-
-        this.location = location;
-        if (tz)
-            timezone = new TimeZone(tz.copy_());
-        else
-            timezone = null;
-        
-        this.store = store;
-        
-        snap = new gtk.snapshot.Snapshot();
-        paintable = paint();
-        time_format = time.format("%x\n%X");
-
-        if ( !ticking_clock_id )
-            ticking_clock_id = timeoutAddSeconds(0, 1, &clock_tick);
-
-        ticking_clocks ~= this;
-    }
-
-    ~this() {
-        import glib.source;
-        import std.algorithm : remove;
-
-        ticking_clocks = ticking_clocks.remove!(el => el == this);
-
-        if ( !ticking_clocks.length && ticking_clock_id != 0 )
-            Source.remove(ticking_clock_id);
-    }
-
-    private bool clock_tick()
-    {
-        foreach (clock; ticking_clocks) {
-            clock.paintable = clock.paint();
-            clock.time_format = clock.time.format("%x\n%X");
-        }
-        /* schedules a redraw */
-        store.itemsChanged(0,0,0);
-        return true;
-    }
-
-    private Paintable paint()
-    {
-        snap = new gtk.snapshot.Snapshot();
-        do_snapshot();
-        return snap.toPaintable();
-    }
-
-    // Crashing code
-    private void do_snapshot()
-    {
-         double width = 100;
-         double height = 100;
-         auto BLACK = new RGBA(0,0,0,1);
-       
-         if (time)
-             time.destroy();
-         if (timezone)
-             time = DateTime.newNow(timezone);
-         else
-             time = DateTime.newNowLocal();
-       
-           snap.save();
-       
-           snap.translate(new Point(width / 2, height / 2));  
-       
-           import gsk.c.types: GskRoundedRect;
-           auto initRect = new Rect();
-           initRect = new Rect(initRect.init_(-50, -50, 100, 100).copy_());
-           GskRoundedRect rr;
-           RoundedRect outline = new RoundedRect(&rr);
-           outline = outline.initFromRect(initRect, 50);
-           snap.appendBorder(outline, [4, 4, 4, 4], [BLACK, BLACK, BLACK, BLACK]);
-       
-           snap.save();
-           snap.rotate(30 * time.getHour() + 0.5 * time.getMinute());
-           initRect = new Rect(initRect.init_(-2, -23, 4, 25).copy_());
-           outline = outline.initFromRect(initRect, 2);
-           snap.pushRoundedClip(outline);
-           snap.appendColor(BLACK, outline.bounds());
-           snap.pop();
-           snap.restore();
-       
-           snap.save();
-           snap.rotate(6 * time.getMinute());
-           initRect = new Rect(initRect.init_(-2, -43, 4, 45).copy_());
-           outline = outline.initFromRect(initRect, 2);
-           snap.pushRoundedClip(outline);
-           snap.appendColor(BLACK, outline.bounds());
-           snap.pop();
-           snap.restore();
-               
-           snap.save();
-           snap.rotate(6 * time.getSecond());
-           initRect = new Rect(initRect.init_(-1, -44, 2, 45).copy_());
-           outline = outline.initFromRect(initRect, 1);
-           snap.pushRoundedClip(outline);
-           snap.appendColor(new RGBA(1,0,0,1), outline.bounds());
-           snap.pop();
-           snap.restore();
-
-           snap.restore();
-    }
-/+ 
-    private void do_snapshot()
-    {
-        double width = 100;
-        double height = 100;
-        auto BLACK = new RGBA(new RGBA(0,0,0,1).copy_());
-
-        if (time)
-            time.destroy();
-        if (timezone)
-            time = new DateTime(DateTime.newNow(timezone).copy_());
-        else
-            time = new DateTime(DateTime.newNowLocal().copy_());
-       
-        snap.save();
-
-        snap.translate(new Point(width / 2, height / 2));
-
-        import gsk.c.functions, gsk.c.types;
-        import graphene.c.functions, graphene.c.types;
-
-        GdkRGBA blackc = GdkRGBA(0,0,0,1);
-        auto black = new RGBA(&blackc);
-
-        auto gpt = graphene_point_t(-50, -50);
-        auto gst = graphene_size_t(100, 100);
-        auto initRectc = graphene_rect_t(gpt, gst);
-        GskRoundedRect outlinec;
-        auto initRect = new Rect(&initRectc);
-        RoundedRect outline = new RoundedRect(&outlinec).initFromRect(initRect, 50);
-        snap.appendBorder(outline, [4, 4, 4, 4], [black, black, black, black]);
-
-        snap.save();
-        snap.rotate(30 * time.getHour() + 0.5 * time.getMinute());
-        gpt = graphene_point_t(-2, -23);
-        gst = graphene_size_t(4, 25);
-        initRectc = graphene_rect_t(gpt, gst);
-        // initRectc = graphene_rect_t(graphene_point_t(-2, -23), graphene_size_t(4, 25));  <== crashes too ! (instead of the 3 preceding lines)
-        gsk_rounded_rect_init_from_rect(&outlinec, &initRectc, 2);
-        outline = new RoundedRect(&outlinec);
-        snap.pushRoundedClip(outline);
-        snap.appendColor(black, outline.bounds());
-        snap.pop();
-        snap.restore();
-
-        snap.save();
-        snap.rotate(6 * time.getMinute());
-        gpt = graphene_point_t(-2, -43);
-        gst = graphene_size_t(4, 45);
-        initRectc = graphene_rect_t(gpt, gst);
-        gsk_rounded_rect_init_from_rect(&outlinec, &initRectc, 2);
-        outline = new RoundedRect(&outlinec);
-        snap.pushRoundedClip(outline);
-        snap.appendColor(black, outline.bounds());
-        snap.pop();
-        snap.restore();
-
-        snap.save();
-        snap.rotate(6 * time.getSecond());
-        gpt = graphene_point_t(-1, -44);
-        gst = graphene_size_t(2, 45);
-        initRectc = graphene_rect_t(gpt, gst);
-        gsk_rounded_rect_init_from_rect(&outlinec, &initRectc, 1);
-        outline = new RoundedRect(&outlinec, No.Take);
-        snap.pushRoundedClip(outline);
-        GdkRGBA redc = GdkRGBA(1,0,0,1);
-        auto red = new RGBA(&redc);
-        snap.appendColor(red, outline.bounds());
-        snap.pop();
-        snap.restore();
-
-        snap.restore();
-    }
-+/
-}
 
 class ClockWindow : ApplicationWindow
 {   
@@ -245,13 +46,6 @@ class ClockWindow : ApplicationWindow
             updateProperty([AccessibleProperty.Label], [new Value("Clocks")]);
         }
         sw.setChild(gridview);
-
-      /* Stress the app so it crashes earlier */
-        auto mdl = cast(ListStore) selModel.getModel();
-        mdl.connectItemsChanged( (uint p, uint a, uint r, ListModel _) {
-            import core.memory : GC;
-            GC.collect();
-        }, Yes.After );
     }
 
     void onFactorySetup(ObjectWrap obj, SignalListItemFactory _)
@@ -268,13 +62,13 @@ class ClockWindow : ApplicationWindow
     {
         auto list_item = cast(ListItem) obj;
         auto box = cast(Box) list_item.getChild();
-        auto clock = cast(GtkClock) list_item.getItem();
+        auto clock = cast(Clock) list_item.getItem();
         
         auto loc = cast(Label) box.getFirstChild();
         loc.setText(clock.location);
 
         auto pic = cast(Picture) loc.getNextSibling();
-        pic.setPaintable(clock.paintable);
+        pic.setPaintable(clock);
 
         auto lbl = cast(Label) pic.getNextSibling();
         lbl.setText(clock.time_format);
@@ -285,37 +79,37 @@ class ClockWindow : ApplicationWindow
         auto store = new ListStore(GTypeEnum.Object);
 
         /* local time */
-        auto clock = new GtkClock("local", null, store);
+        auto clock = new Clock("local", null, store);
         store.append(clock);
         
         /* UTC time */
-        clock = new GtkClock("UTC", TimeZone.newUtc(), store); 
+        clock = new Clock("UTC", TimeZone.newUtc(), store); 
         store.append(clock);
         
         /* A bunch of timezones from everywhere */
-        clock = new GtkClock("San Francisco", TimeZone.newIdentifier("America/Los_Angeles"), store);
+        clock = new Clock("San Francisco", TimeZone.newIdentifier("America/Los_Angeles"), store);
         store.append(clock);
 
-        clock = new GtkClock("Xalapa", TimeZone.newIdentifier("America/Mexico_City"), store);
+        clock = new Clock("Xalapa", TimeZone.newIdentifier("America/Mexico_City"), store);
         store.append(clock);
 
-        clock = new GtkClock("Boston", TimeZone.newIdentifier("America/New_York"), store);
+        clock = new Clock("Boston", TimeZone.newIdentifier("America/New_York"), store);
         store.append(clock);
 
-        clock = new GtkClock("London", TimeZone.newIdentifier("Europe/London"), store);
+        clock = new Clock("London", TimeZone.newIdentifier("Europe/London"), store);
         store.append(clock);
 
-        clock = new GtkClock("Berlin", TimeZone.newIdentifier("Europe/Berlin"), store);
+        clock = new Clock("Berlin", TimeZone.newIdentifier("Europe/Berlin"), store);
         store.append(clock);
 
-        clock = new GtkClock("Moscow", TimeZone.newIdentifier("Europe/Moscow"), store);
+        clock = new Clock("Moscow", TimeZone.newIdentifier("Europe/Moscow"), store);
         store.append(clock);
 
         /* There is an expected half hour offset here ... in few other places too */
-        clock = new GtkClock("New Delhi", TimeZone.newIdentifier("Asia/Kolkata"), store);
+        clock = new Clock("New Delhi", TimeZone.newIdentifier("Asia/Kolkata"), store);
         store.append(clock);
 
-        clock = new GtkClock("Shanghai", TimeZone.newIdentifier("Asia/Shanghai"), store);
+        clock = new Clock("Shanghai", TimeZone.newIdentifier("Asia/Shanghai"), store);
         store.append(clock);
 
         return store;
@@ -366,4 +160,3 @@ int main(string[] args)
 
     return new ClockApp().run(args);
 }
-
